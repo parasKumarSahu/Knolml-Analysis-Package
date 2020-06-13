40fd3589ce045fe662c0fb3869464fc906a7d3ce
paras_kumar <2016csb1047@iitrpr.ac.in>
2020-06-13 16:53:00 +0530

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kml-analysis-parasKumarSahu", 
-    version="0.1.0",
+    version="0.1.1",
     author="Paras Kumar",
     author_email="paraskumardavhehal1@gmail.com",
     description="Wikipedia Analysis Toolkit",
